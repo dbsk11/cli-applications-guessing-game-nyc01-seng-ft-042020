a58c17679513e5f0bab8eb8d6533c199f40c3482
Jessica K <jessikim807@gmail.com>
2020-04-18 23:01:52 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,6 +1,6 @@
 # Code your solution here!
 def run_guessing_game
-  computer_number = rand(6 + 1)
+  computer_number = rand(6) + 1
   puts "Guess a number between 1 and 6"
   user_input = gets.chomp
   if user_input == computer_number.to_s
