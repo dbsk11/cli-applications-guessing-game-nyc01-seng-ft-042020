c8807e21f54375b7ad5fea99216759d020933cbe
Jessica K <jessikim807@gmail.com>
2020-04-18 22:40:44 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -5,10 +5,9 @@ def run_guessing_game
   user_input = gets.chomp
   if user_input == computer_number.to_s
     return "You guessed the correct number!"
+  elsif user_input.downcase == "exit"
+    return "Goodbye!"
   else
     return "Sorry! The computer guessed #{computer_number}."
-  end
-  if user_input.downcase == "exit"
-    return "Goodbye!"
   end
 end
